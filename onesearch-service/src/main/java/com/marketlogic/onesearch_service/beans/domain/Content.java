feda523f9dcef7d8fdd8c1cd917cb0639af416a8
Ponia <sumit.ponia@siemens.com>
2021-09-06 19:38:28 +0530

    Fixed varchar to lob

--- onesearch-service/src/main/java/com/marketlogic/onesearch_service/beans/domain/Content.java
+++ onesearch-service/src/main/java/com/marketlogic/onesearch_service/beans/domain/Content.java
@@ -1,9 +1,12 @@
 package com.marketlogic.onesearch_service.beans.domain;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity(name = "content")
 public class Content {
@@ -12,6 +15,8 @@ public class Content {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long contentId;
 	private Long recordId;
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
 	private String snapshot;
 
 	public Long getRecordId() {
